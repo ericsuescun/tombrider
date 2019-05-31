dce50a62ddd9e5240a9f40aeac1ca10f170da5e9
Eric Suescun <edsuescun@gmail.com>
2019-05-31 17:29:21 -0500

    Se corrige admin para ser boolean false por defecto

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_222324) do
     t.integer "tel"
     t.integer "cel"
     t.integer "name"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
