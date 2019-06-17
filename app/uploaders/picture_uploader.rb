222146ceb71f573fde7cda523447d22f26bcfe21
Eric Suescun <edsuescun@gmail.com>
2019-06-17 12:53:12 -0500

    Se implementa en el uploader limite de 400x400 para las imagenes

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,6 +3,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  include CarrierWave::MiniMagick
+    process resize_to_limit: [400, 400]
+
   if Rails.env.production?
     storage :fog
   else
