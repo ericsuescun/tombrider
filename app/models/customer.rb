0af1b6de4c967aeb935a969728886a4d2d9ff5dd
Eric Suescun <edsuescun@gmail.com>
2019-10-08 18:48:30 -0500

    Elimino verificacion de email y numero telefonico en Customer mientras logro una mejor verificacion de un correo, pero que acepte que este vacio, igual el numero telefonico.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,11 +4,11 @@ class Customer < ApplicationRecord
   has_many :comments, dependent: :destroy
 
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }
-	before_save   :downcase_email
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	# validates :email, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }
+	# before_save   :downcase_email
 
-  	validates :tel, presence: true
+ #  	validates :tel, presence: true
 
   	private
 
