ecc6b100fd18bcc9ab84cb4ce619c1ca6106bc31
Eric Suescun <edsuescun@gmail.com>
2019-10-08 00:37:35 -0500

    Se hace un fix en la busqueda por ID para que se especiique el ID que es un INTEGER para la DB Postgre

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,10 @@ class SearchesController < ApplicationController
 	
 	def index
 		@tombs = Tomb.where("ready like ? ", "Si")
-		@tombs = @tombs.where("id like ? OR title like ? OR description like ? OR entity like ? OR category like ? OR level like ? OR capacity = ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", params[:search].to_i)
+		@tombs = @tombs.where("id like ? OR title like ? OR description like ? OR entity like ? OR category like ? OR level like ? OR capacity = ?", params[:search].to_i, "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", params[:search].to_i)
 		if current_user != nil
 		    if current_user.admin == true
-		      @tombs = Tomb.where("id like ? OR title like ? OR description like ? OR entity like ? OR category like ? OR level like ? OR capacity = ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", params[:search].to_i)
+		      @tombs = Tomb.where("id like ? OR title like ? OR description like ? OR entity like ? OR category like ? OR level like ? OR capacity = ?", params[:search].to_i, "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", params[:search].to_i)
 		    end
 		end
 	end
